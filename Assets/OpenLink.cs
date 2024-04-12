983a8d82050fdbbf685c6223dd342b0f4e16e861
TheWanderer05 <craftingwanderer@gmail.com>
2024-04-12 14:53:42 -0400

    add info panel to upper right, and add link to sphere shader code

---
+++ Assets/OpenLink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+   public void openShaderLink()
+    {
+        Application.OpenURL("https://forum.unity.com/threads/what-is-wrong-with-unitys-spherical-mapping-how-to-fix-it.321205/");
+    }
+}
