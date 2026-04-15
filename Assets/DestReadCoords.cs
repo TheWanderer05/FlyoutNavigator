94e515dddb380c7445dc27433c9da111cd1887cb
TheWanderer05 <craftingwanderer@gmail.com>
2026-04-15 00:21:28 -0400

    Create listeners for reading in destination coordinate fields. Make selecting an airfield read in the destination coords.

---
+++ Assets/DestReadCoords.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DestReadCoords : MonoBehaviour
+{
+    public TMP_InputField latField;
+    public TMP_InputField lonField;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TMP_InputField localLatField = latField.GetComponent<TMP_InputField>();
+        localLatField.onEndEdit.AddListener(delegate { setDestLat(latField); });
+
+        TMP_InputField localLonField = lonField.GetComponent<TMP_InputField>();
+        localLonField.onEndEdit.AddListener(delegate { setDestLon(lonField); });
+    }
+
+    // Note: when selecting an airfield, these don't get called.
+
+    void setDestLat(TMP_InputField field)
+    {
+        if (float.TryParse(field.text, out float latCoords))
+        {
+            string destItemName = field.transform.parent.name;
+            string[] nameParts = destItemName.Split('_');
+            // Second half should be a number
+            if (int.TryParse(nameParts[1], out int destItemIndex))
+            {
+                CalcStart localCS = FindAnyObjectByType<CalcStart>();
+                // Update stuff in CalcStart
+
+                Debug.Log("Dest item " + destItemIndex + " updated Lat coords : " + latCoords);
+            }
+        }
+
+    }
+
+    void setDestLon(TMP_InputField field)
+    {
+        if (float.TryParse(field.text, out float lonCoords))
+        {
+            string destItemName = field.transform.parent.name;
+            string[] nameParts = destItemName.Split('_');
+            // Second half should be a number
+            if (int.TryParse(nameParts[1], out int destItemIndex))
+            {
+                CalcStart localCS = FindAnyObjectByType<CalcStart>();
+                // Update stuff in CalcStart
+
+                Debug.Log("Dest item " + destItemIndex + " updated Lon coords: " + lonCoords);
+            }
+        }
+    }
+
+    // Intended to be called by DestSetAirfield so that the coordinates are actually 
+    // read in when a selection is made and the input fields populate.
+    public void forceSetCoords()
+    {
+        setDestLat(latField);
+        setDestLon(lonField);
+    }
+}
--- Assets/DestSetAirfield.cs
+++ Assets/DestSetAirfield.cs
@@ -21,5 +21,14 @@ public class DestSetAirfield : MonoBehaviour
         // Set lat/lon of this destination item's respective input fields
         GetAirportData localGAD = FindObjectOfType<GetAirportData>();
         localGAD.setDestPoint(dropdown);
+
+        // Update input CalcStart coordinates either in CalcStart or through DestReadCoords (DRC). 
+        // Need the correct instance of DRC if doing that. It should be on the same GameObject as this instance of DestSetAirfield...
+        GameObject parentObj = this.gameObject;
+        DestReadCoords localDRC = parentObj.GetComponent<DestReadCoords>();
+        if (localDRC != null)
+        {
+            localDRC.forceSetCoords();
+        }
     }
 }
